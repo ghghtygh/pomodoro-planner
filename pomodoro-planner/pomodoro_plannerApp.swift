d43f06fac7b65855b9dc2f18ec715e78927a40f5
jhna <jhna@cafe24corp.com>
2025-07-26 20:50:18 +0900

    Initial Commit

---
+++ pomodoro-planner/pomodoro_plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_plannerApp.swift
+//  pomodoro-planner
+//
+//  Created by jhna-macbook on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct pomodoro_plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pomodoro-plannerTests/pomodoro_plannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_plannerTests.swift
+//  pomodoro-plannerTests
+//
+//  Created by jhna-macbook on 7/26/25.
+//
+
+import Testing
+@testable import pomodoro_planner
+
+struct pomodoro_plannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
